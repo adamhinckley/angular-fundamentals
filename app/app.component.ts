7f56e2d30d4aa26d5e83bec2c21e763ac8b19f34
Adam Hinckley <adamhinckley@mac.com>
2019-06-29 15:41:13 -0500

    slytling wiht ngClass nad className bindings

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,19 +9,26 @@ interface Passenger {
 @Component({
   selector: "app-root",
   styleUrls: ["app.component.scss"],
-  // *ngFor is like a for loop in vanilla JS on line 23
-  // everything in the template tag is what's going on under the hood in *ngFor
+  // First one lets you change a single class, second one lets you change as many classes as you need.
   template: `
     <div class="app">
       <h3>Airline Passengers</h3>
       <ul>
-        <template ngFor let-passenger let-i="index" [ngForOf]="passengers">
-          <li>{{ i }}: {{ passenger.fullname }}</li>
-        </template>
+        <li *ngFor="let passenger of passengers; let i = index">
+          <span class="status" [class.checked-in]="passenger.checkedIn"></span>
+          {{ i }}: {{ passenger.fullname }}
+        </li>
       </ul>
       <h3>Airline Passengers</h3>
       <ul>
         <li *ngFor="let passenger of passengers; let i = index">
+          <span
+            class="status"
+            [ngClass]="{
+              'checked-in': passenger.checkedIn,
+              'checked-out': !passenger.checkedIn
+            }"
+          ></span>
           {{ i }}: {{ passenger.fullname }}
         </li>
       </ul>
