5c8a23d28111b59600b21fb038a3b3a082f7e309
Adam Hinckley <adamhinckley@mac.com>
2019-06-29 15:13:20 -0500

    *ngFor and iterating collections

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,29 +1,59 @@
 import { Component } from "@angular/core";
 
+interface Passenger {
+  id: number;
+  fullname: string;
+  checkedIn: boolean;
+}
+
 @Component({
   selector: "app-root",
   styleUrls: ["app.component.scss"],
-  // Lines 12-14 is what's happening under the hood with *
-  // Line 17 *ngIf Only renders when name has length
+  // *ngFor is like a for loop in vanilla JS on line 23
+  // everything in the template tag is what's going on under the hood in *ngFor
   template: `
     <div class="app">
-      <input type="text" [value]="name" (input)="handleChange($event.target.value)" />
-
-      <template [ngIf]="name.length > 2">
-        <div *ngIf="name.length">Searching for... {{ name }}</div>
-      </template>
-
-      <div *ngIf="name.length">Searching for... {{ name }}</div>
+      <h3>Airline Passengers</h3>
+      <ul>
+        <template ngFor let-passenger let-i="index" [ngForOf]="passengers">
+          <li>{{ i }}: {{ passenger.fullname }}</li>
+        </template>
+      </ul>
+      <h3>Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index">
+          {{ i }}: {{ passenger.fullname }}
+        </li>
+      </ul>
     </div>
   `
 })
 export class AppComponent {
-  name: string = "";
-
-  handleChange(value: string) {
-    this.name = value;
-  }
-  handleClick(value: string) {
-    console.log(value);
-  }
+  passengers: Passenger[] = [
+    {
+      id: 1,
+      fullname: "Stephen",
+      checkedIn: true
+    },
+    {
+      id: 2,
+      fullname: "Rose",
+      checkedIn: false
+    },
+    {
+      id: 3,
+      fullname: "James",
+      checkedIn: true
+    },
+    {
+      id: 4,
+      fullname: "Louise",
+      checkedIn: true
+    },
+    {
+      id: 5,
+      fullname: "Tina",
+      checkedIn: false
+    }
+  ];
 }
