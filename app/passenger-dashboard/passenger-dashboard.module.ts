98921b26a67bdcbbb4e003d513cfa0f0b4c6797d
Adam Hinckley <adamhinckley@mac.com>
2019-07-01 20:06:10 -0500

    created a mostly empty  presentational [dumb] component

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
+//containers
 import { PassengerDashboardComponent } from "./containers/passenger-dashboard/passenger-dashboard.component";
 
+//components
+import { PassengerDetailComponent } from "./components/passenger-detail/passenger-detail.component";
+import { PassengerCountComponent } from "./components/passenger-count/passenger-count.component";
+
 @NgModule({
-  declarations: [PassengerDashboardComponent],
+  declarations: [
+    PassengerDashboardComponent,
+    PassengerCountComponent,
+    PassengerDetailComponent
+  ],
   imports: [CommonModule],
+
+  //only the dashboard component needs to be exported because
+  //that's the only one being used at the root of the app
   exports: [PassengerDashboardComponent]
 })
 export class PassengerDashboardModule {}
