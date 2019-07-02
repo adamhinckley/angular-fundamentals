afad12392754681eda49c0775f8837c48cbbba29
Adam Hinckley <adamhinckley@mac.com>
2019-07-01 22:14:04 -0500

    Dynamic @Input values using *NgFor

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -7,22 +7,10 @@ import { Passenger } from "../../models/passenger.interface";
   template: `
     <div>
       <passenger-count [items]="passengers"> </passenger-count>
-      <passenger-detail></passenger-detail>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index">
-          <span class="status" [class.checked-in]="passenger.checkedIn"></span>
-          {{ i }}: {{ passenger.fullname }}
-          <div class="date">
-            check in date:
-            {{
-              passenger.checkInDate
-                ? (passenger.checkInDate | date: yMMMd | uppercase)
-                : "Not checked in."
-            }}
-          </div>
-          <div class="children">Children: {{ passenger.children?.length || 0 }}</div>
-        </li>
-      </ul>
+      <passenger-detail
+        *ngFor="let passenger of passengers"
+        [detail]="passenger"
+      ></passenger-detail>
     </div>
   `
 })
