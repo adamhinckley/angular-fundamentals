3e8e59ae96e6a0d0cabd9d0d45d5e5edeec8dd60
Adam Hinckley <adamhinckley@mac.com>
2019-07-06 10:36:30 -0500

    Select option rendering and use of ngValue

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,9 +1,13 @@
 import { Component, Input } from "@angular/core";
 import { Passenger } from "../../models/passenger.interface";
+import { Baggage } from "../../models/baggage.interface";
 
 @Component({
   selector: "passenger-form",
   styleUrls: ["passenger-form.component.scss"],
+  // Both of the select tags do the same thing
+  //The first one is easier to read
+  //The second one is shorter and a nice use of the API
   template: `
     <form #form="ngForm" novalidate>
       {{ detail | json }}
@@ -29,6 +33,23 @@ import { Passenger } from "../../models/passenger.interface";
         Check in date:
         <input type="number" name="checkInDate" [ngModel]="detail?.checkInDate" />
       </div>
+      <div>
+        Luggage:
+        <select name="baggage" [ngModel]="detail?.baggage">
+          <option
+            *ngFor="let item of baggage"
+            [value]="item.key"
+            [selected]="item.key === detail?.baggage"
+          >
+            {{ item.value }}
+          </option>
+        </select>
+        <select name="baggage" [ngModel]="detail?.baggage">
+          <option *ngFor="let item of baggage" [ngValue]="item.key">
+            {{ item.value }}
+          </option>
+        </select>
+      </div>
       {{ form.value | json }}
     </form>
   `
@@ -36,6 +57,25 @@ import { Passenger } from "../../models/passenger.interface";
 export class PassengerFormComponent {
   @Input()
   detail: Passenger;
+
+  baggage: Baggage[] = [
+    {
+      key: "none",
+      value: "no baggage"
+    },
+    {
+      key: "carry",
+      value: "carry on baggage"
+    },
+    {
+      key: "checked-baggage",
+      value: "checked baggage"
+    },
+    {
+      key: "check-carry",
+      value: "carry on and checked baggage"
+    }
+  ];
 
   toggleCheckIn(checkedIn: boolean) {
     if (checkedIn) {
