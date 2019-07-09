1ff3e9d8af08ca4a1dd0898a8f1799f27514ac5d
Adam Hinckley <adamhinckley@mac.com>
2019-07-08 23:39:27 -0500

    Styles active routes

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,13 +4,21 @@ import { Component } from "@angular/core";
   selector: "app-root",
   styleUrls: ["app.component.scss"],
   template: `
-    <a routerLink="/">
-      Home
-    </a>
-    <a routerLink="/oops">
-      404
-    </a>
-    <div class="app"><router-outlet></router-outlet></div>
+    <div class="app">
+      <nav class="nav">
+        <a
+          routerLink="/"
+          routerLinkActive="active"
+          [routerLinkActiveOptions]="{ exact: true }"
+        >
+          Home
+        </a>
+        <a routerLink="/oops" routerLinkActive="active">
+          404
+        </a>
+      </nav>
+      <router-outlet></router-outlet>
+    </div>
   `
 })
 export class AppComponent {}
